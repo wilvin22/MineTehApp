f96132c180db50766eafc5251f8d6b7bf5f41491
wilvin22 <johnwilvin22@gmail.com>
2026-03-13 00:21:43 +0800

    .

--- app/src/main/java/com/example/mineteh/model/UserBidWithListing.kt
+++ app/src/main/java/com/example/mineteh/model/UserBidWithListing.kt
@@ -1,7 +1,9 @@
 package com.example.mineteh.model
 
+import com.example.mineteh.models.Listing
+
 data class UserBidWithListing(
     val bid: UserBidData,
-    val listing: ItemModel.Listing,
+    val listing: Listing,
     val highestBid: Double
 )
